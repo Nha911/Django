a220d0ca501a6f5df209b95c87dab94ca762e599
Nha911 <selenagamingti2@gmail.com>
2025-05-31 14:31:59 +0700

    Update project files

--- my_project/my_web_app/models.py
+++ my_project/my_web_app/models.py
@@ -6,7 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     
-
     def __str__(self):
         return self.name
 
@@ -18,7 +17,6 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     # or, if using URLField:
     # image = models.URLField(blank=True, null=True)
-
     def __str__(self):
         return self.name
 
